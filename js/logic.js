7eca0dc92815cd838f089c840107ca6e9d0f6f2a
godalming123 <68993177+godalming123@users.noreply.github.com>
2021-01-01 09:29:23 +0000

    create settings by deafualt when page is loaded

--- js/logic.js
+++ js/logic.js
@@ -52,3 +52,29 @@ popupContainer.addEventListener("click", () => {
 	})
 	popupContainer.classList.remove("backdrop");
 }, false)
+
+//create settings
+main.CreateSetting(
+	"animation duration",
+	"--transition",
+	function (settingsInput) {
+		settingsInput.setAttribute("min", 0);
+		settingsInput.setAttribute("max", 4);
+		settingsInput.setAttribute("value", 1);
+	},
+	"s",
+	"range"
+);
+
+main.CreateSetting(
+	"roundness",
+	"--border-radius",
+	function (settingsInput) {
+		settingsInput.setAttribute("step", 0.2)
+		settingsInput.setAttribute("min", 0);
+		settingsInput.setAttribute("max", 4);
+		settingsInput.setAttribute("value", 1);
+	},
+	"vh",
+	"range"
+);
