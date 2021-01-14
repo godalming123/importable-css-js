491e61619401c364c49a8f5d1e2e51cf43a240db
godalming123 <68993177+godalming123@users.noreply.github.com>
2021-01-14 12:16:38 +0000

    importer.js works

--- js/importer.js
+++ js/importer.js
@@ -54,11 +54,11 @@ function importJsOrCss(file, importTag) {
 	else if (importEnding == "js") { importJs(path, importType); }
 }
 
-for (importTag of importTags) {
+for (var importTag of importTags) {
 	importTag.style.display = "none";
 	importTag.innerHTML.split(" ").forEach((import_) => {
 		//if import is a keywaord like popup than we can import everything needed to implement that
-		for (importKeyword of importKeywords) {
+		for (var importKeyword of importKeywords) {
 			if (import_ == importKeyword[0]) {
 				importKeyword[1].forEach(item => { importJsOrCss( item, importTag ) });
 				return; //!skip iteration of loop 5 lines above
